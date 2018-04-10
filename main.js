7777163db798c3206627b7c1681b1a5788cac9a2
Santon <anton0102@mail.ru>
2018-04-10 15:51:34 +0300

    gsheet

--- main.js
+++ main.js
@@ -2,20 +2,47 @@ var API_KEY = "AIzaSyBwheoYt0INYUUcupBUawA2JBLfp3SiVMY";
 
 var spreadsheetID = "1t0Q8SdgaUX7w62Vb2AzKHowKkm3i2_U3UKpuUxgYAak" ;
 
-var range = "Контакты!b2:g93";
+var range = "Контакты!b1:h93";
 
 var API_URL = "https://sheets.googleapis.com/v4/spreadsheets/"
     + spreadsheetID
     + "/values/"
     + range
     + '?key=' + API_KEY;
-//отпправка запроса и обработка ответа API
 
-
+// отпправка запроса и обработка ответа API
 fetch(API_URL)
     .then(response => response.json()) // обрабатываем ответ апи
     .then(data => {
-        console.log(data.values); // выводим содержание таблицы
-    })
+        var s = data.values;
+        find (s, "Roman")
+    
+})
 
-// https://sheets.googleapis.com/v4/spreadsheets/1EXOVPVfmpZj_bSR3e1RwMbmB_7a_SUod0nv6PG80dpA/values/A1:B16?key=AIzaSyBwheoYt0INYUUcupBUawA2JBLfp3SiVMY
+// Поиск годфазера среди  всей таблици
+function find(arr, value) {
+    
+    for (var str = 0; str < arr.length; str++)
+        if (arr[str][0] == value) {
+            var strI = String (str);
+            console.log (arr[strI]);
+        }
+}
+
+
+
+// // Поиск нажатого чекбокса
+// var CB_checked = document.querySelector('.gf');
+// CB_checked.addEventListener ('checked', function(){
+//   ///////что то надо ввести
+// })
+
+// var element = document.querySelector('.gf'); 
+// alert (element);
+// debugger
+
+// if (element.addEventListener('click',function(){
+//     console.log (element.id);
+// }));
+
+
